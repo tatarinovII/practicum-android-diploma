438fcfc99d3b64280f667f09f212a2590e6de893
tatarinovii <ilya.tatarinov77@gmail.com>
2026-06-16 14:33:10 +0300

    - создал файл Dimens.kt - добавил основные размеры: текста, отступов и LineHeight

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val PaddingBase = 16.dp
+val PaddingBig = 20.dp
+val PaddingMedium = 12.dp
+val PaddingSmall = 8.dp
+
+val LineHeightBig = 38.sp
+val LineHeightMedium = 26.sp
+val LineHeightSmall = 19.sp
+
+val FontSizeBig = 32.sp
+val FontSizeMedium = 22.sp
+val FontSizeBase = 16.sp
+val FontSizeSmall = 12.sp
