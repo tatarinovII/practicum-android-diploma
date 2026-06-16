4b8b0c443b23aca34194e61f1fbc5fae4e63d877
tatarinovii <ilya.tatarinov77@gmail.com>
2026-06-16 14:39:21 +0300

    - добавил новые шрифты - создал FontFamily

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/PracticumFontFamily.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import ru.practicum.android.diploma.R
+
+val PracticumFontFamily = FontFamily(
+    Font(R.font.ys_display_bold, FontWeight.Bold),
+    Font(R.font.ys_display_medium, FontWeight.Medium),
+    Font(R.font.ys_display_regular, FontWeight.Normal),
+)
