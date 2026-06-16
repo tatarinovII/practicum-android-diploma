a79797c9ce4826082ab5cec8ed8246bb50fd883a
tatarinovii <ilya.tatarinov77@gmail.com>
2026-06-16 14:21:45 +0300

    - создал файл Colors.kt - добавил цвета приложения

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Colors.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF1A1B22)
+val White = Color(0xFFFDFDFD)
+val Blue = Color(0xFF3772E7)
+val Red = Color(0xFFF56B6C)
+val Grey = Color(0xFFAEAFB4)
+val LightGrey = Color(0xFFE6E8EB)
+val Background = Color(0x1A1B2280)
