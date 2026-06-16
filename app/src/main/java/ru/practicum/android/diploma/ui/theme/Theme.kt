0fa8a01012f8dc8e6a02957d14a466e92ddd6270
tatarinovii <ilya.tatarinov77@gmail.com>
2026-06-16 15:07:43 +0300

    - добавил тему приложения

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = White,
+    secondary = Black,
+    tertiary = Grey
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Black,
+    secondary = White,
+    tertiary = LightGrey
+)
+
+@Composable
+fun MyAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
